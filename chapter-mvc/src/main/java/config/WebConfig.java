975a5b874e04aa01ad349bcfa18e7db78fd8db2f
Young <young_fine@163.com>
2018-04-06 13:59:07 +0800

    spring mvc view

--- chapter-mvc/src/main/java/config/WebConfig.java
+++ chapter-mvc/src/main/java/config/WebConfig.java
@@ -1,10 +1,23 @@
 package config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import spittr.controller.HomeController;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @ComponentScan(basePackages = {"spittr.controller"})
 public class WebConfig {
+    //视图解析
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        //标签支持
+        viewResolver.setViewClass(JstlView.class);
+        return viewResolver;
+    }
 }
---
+++ chapter-mvc/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2018/4/6
+  Time: 12:22
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="sf"%>
+<%@ taglib uri="http://www.springframework.org/tags" prefix="s"%>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>Hello world!</h1>
+<s:url value="/spittr/register">
+    <s:param name="from" value="index"/>
+</s:url>
+</body>
+</html>
